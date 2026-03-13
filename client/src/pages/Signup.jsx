0c5232eda3a05e288169e23da4034c0c2b7e1022
kundandewase <165580237+kundandewase@users.noreply.github.com>
2026-03-13 11:55:35 +0530

    fix signup form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,27 @@
 function Signup() {
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Signup Page</h1>
+
+      <form>
+        <div>
+          <label>Email:</label><br />
+          <input type="email" placeholder="Enter email" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password:</label><br />
+          <input type="password" placeholder="Enter password" />
+        </div>
+
+        <br />
+
+        <button type="submit">Signup</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
